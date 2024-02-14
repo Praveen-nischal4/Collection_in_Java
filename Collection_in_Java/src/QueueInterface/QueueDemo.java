2efcb45b37629ff9039f34ccd6aa15afc4e97577
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-14 21:36:46 +0000

    14-02-24

---
+++ Collection_in_Java/src/QueueInterface/QueueDemo.java
@@ -0,0 +1,20 @@
+package QueueInterface;
+import java.util.*;
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+ 
+		Queue<Integer> lst = new LinkedList<>();
+		
+		//store elements in queue		
+	for(int i=1;i<=5;i++)
+	{
+		lst.offer(i);
+	}
+		
+		//retrieve elements of linkedlist
+       System.out.println(lst);
+	}
+
+}
