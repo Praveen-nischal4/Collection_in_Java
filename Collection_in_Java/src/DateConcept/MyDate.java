e8318afa73fde60349c5e284e59bb9389c535098
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-15 22:01:49 +0000

    15-02-24

---
+++ Collection_in_Java/src/DateConcept/MyDate.java
@@ -0,0 +1,19 @@
+package DateConcept;
+import java.text.DateFormat;
+import java.util.*;
+import java.text.*;
+
+public class MyDate {
+
+	public static void main(String[] args) {
+	 
+		Date d = new Date();
+		//System.out.println(d);
+		
+		DateFormat fmt =DateFormat.getDateInstance(DateFormat.SHORT,Locale.UK);
+		String str =fmt.format(d);
+		System.out.println(str);
+
+	}
+
+}
